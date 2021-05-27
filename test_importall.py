d8ed30fab51d46587b5140a5e21b9eea87d56489
MapleCCC <littlelittlemaple@gmail.com>
2021-05-28 04:18:53 +0800

    Organize fixtures

--- test_importall.py
+++ test_importall.py
@@ -9,28 +9,40 @@ from typing import Any
 import pytest
 
 
-@pytest.fixture(scope="function", autouse=True)
-def isolate_environement():
+@pytest.fixture()
+def isolate_globals():
+    globals_dict = globals()
+    original_globals = dict(globals_dict)
+    yield
+    globals_dict.clear()
+    globals_dict |= original_globals
+
+
+@pytest.fixture()
+def isolate_sys_modules():
+    original_sys_modules = dict(sys.modules)
+    yield
+    sys.modules.clear()
+    sys.modules |= original_sys_modules
+
+
+@pytest.fixture()
+def isolate_os_environ():
+    original_os_environ = dict(os.environ)
+    yield
+    os.environ.clear()
+    os.environ |= original_os_environ
+
+
+@pytest.fixture(autouse=True)
+def isolate_environement(isolate_globals, isolate_sys_modules, isolate_os_environ):
+    """
+    Before the test runs, backup the environment.
+    After the test runs, restore the environment.
+    """
 
     # Reference: test.support.CleanImport() function implementation
     # https://github.com/python/cpython/blob/v3.9.0/Lib/test/support/__init__.py#L1241
-
-    globals_dict = globals()
-
-    # Backup environement
-    original_globals = dict(globals_dict)
-    original_sys_modules = dict(sys.modules)
-    original_os_environ = dict(os.environ)
-
-    yield
-
-    # Restore environment
-    globals_dict.clear()
-    globals_dict |= original_globals
-    sys.modules.clear()
-    sys.modules |= original_sys_modules
-    os.environ.clear()
-    os.environ |= original_os_environ
 
 
 def eval_name(name: str) -> Any:
