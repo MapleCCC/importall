ee1d7a3b64ed336f54aba35b429e5eec8c86edc3
MapleCCC <littlelittlemaple@gmail.com>
2021-09-07 07:40:38 +0800

    feat: `python -m importall [script]` to run the script with every available names from standard libraries injected into the script's namespace

--- src/importall/__main__.py
+++ src/importall/__main__.py
@@ -1,14 +1,16 @@
 """
-Script to launch an importall REPL, within which every available names from standard
-libraries are already imported.
+A script to either launch an importall REPL, within which every available names from
+standard libraries are already imported, or run a Python script, with every available
+names from standard libraries injected into the script's namespace.
 
-Usage: `python -m importall`
+Usage: `python -m importall [script]`
 """
 
 initial_globals = globals().copy()
 
 import code
 import sys
+from pathlib import Path
 
 import colorama
 from colorama import Fore, Style
@@ -29,6 +31,18 @@ def bright_green(s: str) -> str:
 
 def main() -> None:
 
+    assert len(sys.argv[1:]) <= 1, "at most one argument is accepted"
+
+    inject_globals = initial_globals | get_all_symbols()
+
+    # If there is a script file as command line argument, run it, instead of launching
+    # an interactive shell.
+    if sys.argv[1:]:
+        script = Path(sys.argv[1])
+        text = script.read_text(encoding="utf-8")
+        exec(compile(text, script, "exec"), inject_globals)
+        return
+
     prompt = getattr(sys, "ps1", ">>> ")
 
     banner = (
@@ -42,11 +56,9 @@ def main() -> None:
         + bright_green("import everything!🚀✨")
     )
 
-    local = initial_globals | get_all_symbols()
-
     exitmsg = "exiting importall REPL..."
 
-    code.interact(banner=banner, local=local, exitmsg=exitmsg)
+    code.interact(banner=banner, local=inject_globals, exitmsg=exitmsg)
 
 
 if __name__ == "__main__":
