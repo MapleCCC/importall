ac65ce5bf827313437397fb12f56b732ef3eddce
MapleCCC <littlelittlemaple@gmail.com>
2021-09-11 12:58:57 +0800

    refactor: generate.py

--- src/importall/generate.py
+++ src/importall/generate.py
@@ -21,15 +21,12 @@ MODULES_WITH_ZERO_TOP_LEVEL_PUBLIC_NAMES = frozenset(
 )
 
 
-def main() -> None:
+def generate_stdlib_public_names() -> dict[str, list[str]]:
 
-    ver = ".".join(str(c) for c in sys.version_info[:2])
-    file = Path(__file__).with_name("stdlib_public_names") / (ver + ".json")
+    stdlib_public_names: dict[str, list[str]] = {}
 
-    data: dict[str, list[str]] = {}
-
-    # FIXME we should not only use IMPORTABLE_STDLIB_MODULES, because the list should be much more general
     for module_name in tqdm(
+        # FIXME we should not only use IMPORTABLE_STDLIB_MODULES, because the list should be much more general
         IMPORTABLE_STDLIB_MODULES,
         desc="Generating static list of stdlib public names",
         # TODO which style is better: "5.42module/s" or "5.42modules/s" ?
@@ -49,14 +46,31 @@ def main() -> None:
         if module_name not in MODULES_WITH_ZERO_TOP_LEVEL_PUBLIC_NAMES:
             assert public_names, f"{module_name} has zero top level public names"
 
-        data[module_name] = sorted(public_names)
+        stdlib_public_names[module_name] = sorted(public_names)
 
-    file.write_text(json.dumps(data, indent=2, sort_keys=True), encoding="utf-8")
+    return stdlib_public_names
 
+
+def reformat_json_file(file: Path) -> None:
     prettier_executable = "prettier.cmd" if os.name == "nt" else "prettier"
-    subprocess.check_call(
-        [prettier_executable, "--end-of-line", "auto", "--write", str(file)]
-    )
+    options = ["--end-of-line", "auto", "--write"]
+    subprocess.check_call([prettier_executable, *options, str(file)])
+
+
+def main() -> None:
+
+    ver = ".".join(str(c) for c in sys.version_info[:2])
+    file = Path(__file__).with_name("stdlib_public_names") / (ver + ".json")
+
+    stdlib_public_names = generate_stdlib_public_names()
+
+    json_style = {"indent": 2, "sort_keys": True}
+
+    json_text = json.dumps(stdlib_public_names, **json_style)
+
+    file.write_text(json_text, encoding="utf-8")
+
+    reformat_json_file(file)
 
 
 if __name__ == "__main__":
