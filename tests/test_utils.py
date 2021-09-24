f1e3029efb7db90afcbd8321805f06b5465ad995
MapleCCC <littlelittlemaple@gmail.com>
2021-09-24 22:16:28 +0800

    test: update unit tests for `run_in_new_interpreter()`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -26,14 +26,20 @@ def test_provide_lazy_version(x) -> None:
     assert inc(x, lazy=True) == inc(x, lazy=False) == inc.__wrapped__(x) == x + 1
 
 
+@pytest.mark.asyncio
 class TestRunInNewInterpreter:
     """Unit tests for `run_in_new_interpreter()`"""
 
     @settings(max_examples=5, deadline=3000)
-    @given(integers())
-    def test_normal_call(self, x: int) -> None:
-        assert x + 1 == run_in_new_interpreter(operator.add, x, 1)
+    @given(integers())  # type: ignore # FIXME `@given()`'s type annotation should be updated to accept async function.
+    async def test_normal_call(self, x: int) -> None:
 
-    def test_fresh_environment(self) -> None:
+        assert x + 1 == await run_in_new_interpreter(operator.add, x, 1)
+
+    async def test_fresh_environment(self) -> None:
+
         sys.modules["pytest"] = pytest
-        assert run_in_new_interpreter(eval, "'pytest' not in __import__('sys').modules")
+
+        assert await run_in_new_interpreter(
+            eval, "'pytest' not in __import__('sys').modules"
+        )
